22619cdc660b18084eb08414cf0f8fc658ef6c62
FixterSpb <Fixter.spb@gmail.com>
2020-05-29 17:01:22 +0300

    Начало верстки
    Сверстан header

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>New automation</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="top">
+            <header class="header">
+                <div class="header__topline">
+                    <div class="logo logo_header">
+                        <a href="/" class="logo__link">
+                            <img src="img/logo.png" alt="icon logo" class="logo__img">
+                        </a>
+                    </div>
+                    <div class="address-book">
+                        <a href="#" class="address-book__link">
+                            <img src="img/address_book.png" alt="icon" class="address-book__img">
+                        </a>
+                    </div>
+                </div>
+                <div class="header__content">
+                    <h1 class="header__title">
+                        New Automation<br>
+                        Tool for Your
+                    </h1>
+                    <p class="header__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus tristique vulputate
+                        ultrices
+                        ut
+                        mauris tellus at. Posuere sollicitudin odio tellus elit.
+                    </p>
+
+                    <a href="#" class="button">
+                        See Our Project
+                    </a>
+
+                </div>
+            </header>
+
+            <section class="content">
+                <h2 class="content__title">
+                    What we do to help our client grow in digital eraz
+                </h2>
+            </section>
+
+        </div>
+        <footer class="footer">
+        </footer>
+    </div>
+</body>
+
+</html>
