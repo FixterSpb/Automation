1cf31655bf39f0e52d506a7a7460eaabfb204ec0
Mladshev_Evgeniy <Fixter.spb@gmail.com>
2020-06-01 03:29:47 +0300

    Сверстана основная часть главной страницы

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/style.css?ver=1.1">
     <title>New automation</title>
 </head>
 
@@ -45,9 +47,84 @@
             </header>
 
             <section class="content">
-                <h2 class="content__title">
-                    What we do to help our client grow in digital eraz
-                </h2>
+                <header class="content__header">
+                    <h2 class="content__title">
+                        What we do to help our client grow in digital eraz
+                    </h2>
+                </header>
+
+                <div class="row center">
+                    <article class="col-xl-4 col-lg-12">
+                        <div class="content__box">
+                            <div class="content__img content__img_1"></div>
+                            <h3 class="content__name">Make Your business To Be Better Famous In Internet</h3>
+                            <p class="content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <a href="#" class="content__link">Learn more <img src="img/arrow.png" alt="icon"></a>
+                        </div>
+                    </article>
+
+                    <article class="col-xl-4 col-lg-12">
+                        <div class="content__box">
+                            <div class="content__img content__img_2"></div>
+                            <h3 class="content__name">Bring Technology To Your Comfrotable Home</h3>
+                            <p class="content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <a href="#" class="content__link">Learn more <img src="img/arrow.png" alt="icon"></a>
+                        </div>
+                    </article>
+
+                    <article class="col-xl-4 col-lg-12">
+                        <div class="content__box">
+                            <div class="content__img content__img_3"></div>
+                            <h3 class="content__name">Make Your business To Be Better Famous In Internet</h3>
+                            <p class="content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <a href="#" class="content__link">Learn more <img src="img/arrow.png" alt="icon"></a>
+                        </div>
+                    </article>
+                </div>
+            </section>
+
+            <section class="projects">
+                <header class="projects__header">
+                    <h2 class="projects__title">
+                        Our Finished Project
+                    </h2>
+                    <p class="projects__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
+                </header>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="projects__box">
+                            <img src="img/project_1.png" alt="project" class="projects__img">
+                            <h3 class="projects__name projects__name_right">
+                                Smart Home Installation
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="projects__box projects__box_2">
+                            <img src="img/project_2.png" alt="project" class="projects__img">
+                            <h3 class="projects__name">
+                                Sparklite App
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="projects__box">
+                            <img src="img/project_3.png" alt="project" class="projects__img">
+                            <h3 class="projects__name">
+                                Car-Rapetition App
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
             </section>
 
         </div>
